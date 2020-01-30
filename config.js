0bee775b7ebc4c7e2f920cc6802d757891c86018
Luiz <luizplayer2016@hotmail.com>
2020-01-30 17:36:01 -0300

    "refix"

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.bra_id = "A2E4A385-43AC-E511-80D4-E4115BDFF975";
-config.channel_id= "670653074737594370";
+config.channel_id= "565607843139420169";
 config.token = process.env.token;
 config.AuthStr = "Bearer ".concat(process.env.apiToken);
 
